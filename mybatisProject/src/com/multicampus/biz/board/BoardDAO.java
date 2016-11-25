832d564bc22efd5a31747732781d364ea622e87a
charlieshin <chshin33@gmail.com>
2016-11-25 16:52:19 +0900

    Mabisis실습

---
+++ mybatisProject/src/com/multicampus/biz/board/BoardDAO.java
@@ -0,0 +1,38 @@
+package com.multicampus.biz.board;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.multicampus.biz.util.SqlSessionFactoryBean;
+
+public class BoardDAO {
+	private SqlSession mybatis;
+	
+	public BoardDAO() {
+		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
+	}
+	
+	public void insertBoard(BoardVO vo) {
+		mybatis.insert("boardDAO.insertBoard", vo);
+		mybatis.commit();
+	}
+
+	public void updateBoard(BoardVO vo) {
+		mybatis.update("boardDAO.updateBoard", vo);
+		mybatis.commit();
+	}
+
+	public void deleteBoard(BoardVO vo) {
+		mybatis.delete("boardDAO.deleteBoard", vo);
+		mybatis.commit();
+	}
+
+	public BoardVO getBoard(BoardVO vo) {
+		return (BoardVO) mybatis.selectOne("boardDAO.getBoard", vo);
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return mybatis.selectList("boardDAO.getBoardList", vo);
+	}
+}
